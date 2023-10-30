310be09ba1472cb3ce08b44ab2afc15ae809be58
zazering <108462381+zazering@users.noreply.github.com>
2023-10-30 19:05:51 +0300

    Update task_6.py

--- task_6.py
+++ task_6.py
@@ -1,9 +1,5 @@
-# task 6
-a, b, c = map(float, input().split())
-d = b ** 2 - 4 * a * c
-x1 = (-b + d ** 0.5) / (2 * a)
-x2 = (-b - d ** 0.5) / (2 * a)
-if x1 < x2:
-    print(f"{x1:.3f} {x2:.3f}")
+x1, y1, x2, y2 = map(int, input("enter coordinates: ").split())
+if abs(x1 - x2) == abs(y1 - y2):
+    print("YES")
 else:
-    print(f"{x2:.3f} {x1:.3f}")
+    print("NO")
